7bc3af3e300e35757d0cd9638de0080ae8f8280d
matti <18363284+kamatti@users.noreply.github.com>
2024-08-15 20:01:36 +0900

    演習課題の関数宣言と動作確認用のコードを追加

--- rust_by_example/_3_1_structure/src/main.rs
+++ rust_by_example/_3_1_structure/src/main.rs
@@ -6,15 +6,78 @@ struct Person {
 
 struct Pair(i32, f32);
 
+#[derive(PartialEq)]
 struct Point {
     x: f32,
     y: f32,
 }
 
+#[derive(PartialEq)]
 struct Rectangle {
     top_left: Point,
     bottom_right: Point,
 }
 
+// 演習1
+// Rectangleの面積を計算
+fn rect_area(rect: Rectangle) -> f32 {
+    return 0.0;
+}
+
+// 演習2
+// pointを左下の座標とする幅と高さがlengthとなるRectangleを生成
+fn square(point: Point, length: f32) -> Rectangle {
+    return Rectangle{ top_left: Point { x: 0.0, y: 1.0}, bottom_right: Point { x: 1.0, y: 0.0}};
+}
+
 fn main() {
+    // 2*5の長方形
+    // 座標は全て自然数
+    let a = Rectangle {
+        top_left: Point { x: 0.0, y: 5.0},
+        bottom_right: Point { x: 2.0, y: 0.0}
+    };
+
+    // 2*5の長方形
+    // 座標に負の数を含む
+    let b = Rectangle {
+        top_left: Point { x: -1.0, y: 3.0},
+        bottom_right: Point { x: 1.0, y: -2.0}
+    };
+
+    // 2.3*4.8の長方形
+    let c = Rectangle {
+        top_left: Point { x: 0.0, y: 4.8},
+        bottom_right: Point { x: 2.3, y: 0.0}
+    };
+
+    // rect_areaの動作確認
+    std::assert!(rect_area(a) == 10.0);
+    std::assert!(rect_area(b) == 10.0);
+    std::assert!(rect_area(c) == 11.04);
+
+    // 3*3の正方形
+    // 座標は全て自然数
+    let d = Rectangle {
+        top_left: Point { x: 0.0, y: 3.0},
+        bottom_right: Point { x: 3.0, y: 0.0}
+    };
+
+    // 3*3の正方形
+    // 座標に負の数を含む
+    let e = Rectangle {
+        top_left: Point { x: -1.0, y: 1.0},
+        bottom_right: Point { x: 2.0, y: -2.0}
+    };
+
+    // 1.5*1.5の正方形
+    let f = Rectangle {
+        top_left: Point { x: 0.0, y: 1.5},
+        bottom_right: Point { x: 1.5, y: 0.0}
+    };
+
+    std::assert!(square(Point{ x: 0.0, y: 0.0 }, 3.0) == d);
+    std::assert!(square(Point{ x: -1.0, y: -2.0 }, 3.0) == e);
+    std::assert!(square(Point{ x: 0.0, y: 0.0 }, 1.5) == f);
+    
 }
