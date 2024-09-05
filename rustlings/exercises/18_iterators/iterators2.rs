9c22cf133ed826e1ac8d6f45cf4d4248bff3d45d
kamatti <18363284+kamatti@users.noreply.github.com>
2024-09-05 18:46:27 +0900

    rustlings exercise 18 完了

--- rustlings/exercises/18_iterators/iterators2.rs
+++ rustlings/exercises/18_iterators/iterators2.rs
@@ -7,7 +7,7 @@ fn capitalize_first(input: &str) -> String {
     let mut chars = input.chars();
     match chars.next() {
         None => String::new(),
-        Some(first) => todo!(),
+        Some(first) => first.to_ascii_uppercase().to_string() + &input[1..],
     }
 }
 
@@ -16,6 +16,12 @@ fn capitalize_first(input: &str) -> String {
 // ["hello", "world"] -> ["Hello", "World"]
 fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
     // ???
+    let mut capitalized = Vec::<String>::new();
+    for w in words {
+        capitalized.push(capitalize_first(w));
+    }
+
+    capitalized
 }
 
 // TODO: Apply the `capitalize_first` function again to a slice of string
@@ -23,6 +29,12 @@ fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
 // ["hello", " ", "world"] -> "Hello World"
 fn capitalize_words_string(words: &[&str]) -> String {
     // ???
+    let mut concated = String::new();
+    for w in words {
+        concated += &capitalize_first(w);
+    }
+
+    concated
 }
 
 fn main() {
